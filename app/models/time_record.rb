e80896052091f15307e60ad4eb7d016a2c3292ee
DiegoMero-UST <diegom@usmarttech.com>
2025-02-03 14:25:31 -0500

    Add horas trabajadas

--- app/models/time_record.rb
+++ app/models/time_record.rb
@@ -4,4 +4,14 @@ class TimeRecord < ApplicationRecord
   validates :date, presence: true
   validates :clock_in, presence: true
   validates :user, presence: true
+
+  def worked_hours
+    return "En curso" unless clock_out
+    
+    duration = clock_out - clock_in
+    hours = (duration / 1.hour).floor
+    minutes = ((duration % 1.hour) / 1.minute).round
+    
+    format("%02d:%02d", hours, minutes)
+  end
 end 
